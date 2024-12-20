bb139870c1f8b3c1808bca04d439a19b86ab639b
Helen Haveloh <h.havelohh@gmail.com>
2024-12-20 12:36:19 +0100

    added gallery scripts

--- script.js
+++ script.js
@@ -1,172 +1,64 @@
-// console.log("Js Script geladen");
-
-// const header = document.getElementById('header-example')
-// // console.log(header);
-// header.innerHTML = "Das ist ein Test";
-// console.log(header)
-
-// if (header){
-//     console.log("Wir haben eine Überschrift");
-// } else {
-//     console.log("Wir haben keine Überschrift");
-// }
+console.log("Script loaded");
+console.log("Das ist der zweite Aufruf");
+const userlist = document.getElementById('user-list');
+const imageList = document.getElementById('image-list');
 
 
+// Define an array for users
+let users = []; 
+let images = []; // to be filled with images from api endpoint https://jsonplaceholder.typicode.com/photos
 
-// const firstListElement = document.getElementById('first-list-element');
-// const checkbox = document.getElementById('checkbox');
-// const updateButton = document.getElementById('updateButton');
-// console.log(updateButton);
-// function () {} === () => {}
-// updateButton.addEventListener('click',function () {
-//     console.log("Button gedrückt");
-//     if (checkbox.checked){
-//         console.log(checkbox.checked);
-//         firstListElement.style.backgroundColor = "green";
-//     } else {
-//         console.log(checkbox.checked);
-//         firstListElement.style.backgroundColor = "red";
-//     }
-// });
-// console.log(checkbox.checked)
-// updateListItems();
-
-// function updateListItems(){
-//     console.log(checkbox.checked);
-//     if(header){
-//         firstListElement.style.color = "rgb(0,0,0)"
-//         firstListElement.style.textDecoration = "line-through";
-//     }
-// }
-// const secondListElement = document.getElementById('second-list-element');
-//  console.log(secondListElement.style);
-
-
-// const lists = document.querySelectorAll('.list');
-// console.log(lists);
-
-// const listWithId = document.getElementById('list');
-// console.log(listWithId);
-
-// Hier beginnt unser Skript
-const list = document.querySelector('.list[id="list"]')
-let items = [];
-
-// Funktion zum Rendern
-function renderItems(){
-    // console.log(list);
-    list.innerHTML = '';
-    // console.log("leere Liste", list.innerHTML);
-    items.forEach((item) => {
-        // list items jeweils erstellen
-        const listItem = document.createElement('li');
-        listItem.innerHTML = item.text;
-        listItem.classList.add('list-item');
-        // console.log(listItem);
+// define async function to fetch users data
+async function fetchUserData(){
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        // console.log(typeof(response);
+        usersData = await response.json();
+        // console.log(usersData);
+        users = usersData;
+        console.log(users);
+        renderUsers();
         
-        // Checkbox jeweils erstellen
-        const checkboxForItem = document.createElement('input');
-        checkboxForItem.type = 'checkbox';
-        checkboxForItem.checked = false;
-        checkboxForItem.classList.add('checkbox-item');
-
-        checkboxForItem.addEventListener('change', () => {
-            // console.log(checkboxForItem.checked);
-            if (checkboxForItem.checked){
-                listItem.style.textDecoration = 'line-through';
-            } else {
-                listItem.style.textDecoration = 'none';
-            }
-        })
-
-        // dem DOM hinzufügen
-        list.appendChild(listItem);
-        listItem.appendChild(checkboxForItem);
-    })
-    saveItems();
-    // console.log("gefüllte Liste", list.innerHTMl);
-}
-// Items hinzufügen
-const button = document.getElementById('add-item-button')
-const input = document.getElementById('input-item');
-button.addEventListener('click', () => {
-    console.log("Hinzufügen-Button gedrückt");
-    const newItem = {
-        id: Date.now(),
-        text: input.value
+    } catch (error) {
+        console.log("Wir bekommen beim Aufruf der Users-APi den folgenden Fehler", error);
     }
-    items.push(newItem);
-    input.value = '';
-    // console.log(items);
-    renderItems();
-})
-
-
-// Funktion zum Speichern im localstorage
-function saveItems(){
-    // console.log(items.length === 0)
-    // console.log(items);
-    if (items.length === 0){
-        // console.log("klappt");
-        return;
-    };
-    const stringifiedItems = JSON.stringify(items);
-    // console.log(typeof(stringifiedItems));
-    localStorage.setItem("items", stringifiedItems);
 }
 
-// Funktion zum Laden der Items
-function loadItems(){
-    console.log("loading storage");
-    const itemsFromLocalStorage = localStorage.getItem('items');
-    if (!itemsFromLocalStorage){
-        items = [];
-    } else {
-        items = JSON.parse(itemsFromLocalStorage);
+// define async function to fetch image data
+async function fetchImageData() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+        imageData = await response.json();
+        images = imageData.slice(0, 10);
+        // console.log(images);
+        renderImages();
+    } catch (error) {
+        console.log("Wir bekommen beim Fetching der Image API folgenden Fehler: ", error)
     }
-    // console.log(itemsFromLocalStorage);
-    // console.log(items);
+    
 }
 
-// Ablauf der Funktionsaufrufe 
-loadItems();
-renderItems();
-saveItems();
+fetchUserData();
+fetchImageData();
 
+function renderUsers(){
+    users.forEach((user) => {
+        const userItem = document.createElement('li');
+        userItem.innerHTML = user.name;
+        // console.log(user);
 
+        userlist.appendChild(userItem);
+    });
+}
 
+function renderImages(){
+    images.forEach((image) => {
+        const imageItem = document.createElement('img');
+        console.log(imageItem);
+        imageItem.setAttribute('src', image.url)
+        imageList.appendChild(imageItem)
 
+    });
+}
 
-
-
-
-// console.log("ende erreicht");
-// add-item-button.addEventListener
-
-
-
-// alternative Möglichkeiten, eine for-Schleife zu definieren
-// for (let i=0; i <items.length; i++){
-//     const listItem = document.createElement('li');
-//     listItem.innerHTML = items[i];
-//     console.log(listItem);
-// }
-
-// for (const item in items){
-//     const listItem = document.createElement('li');
-//     listItem.innerHTML = item;
-//     console.log(listItem);
-// }
-
-// for(item of items){
-//     const listItem = document.createElement('li');
-//     listItem.innerHTML = item;
-//     console.log(listItem);
-// }
-
-// items.push("Wasser");
-// console.log(items)
-
-
-
-
+// showUsers();
